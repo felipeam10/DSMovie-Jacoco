d7887a26b96e157af7c8800cb9e4f81a72c59d47
felipeam10 <felipeam10@hotmail.com>
2024-01-19 02:48:53 -0300

    test findAllShouldReturnPagedMovieDTO

--- src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
@@ -1,18 +1,53 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(SpringExtension.class)
 public class MovieServiceTests {
 	
 	@InjectMocks
 	private MovieService service;
+	@Mock
+	private MovieRepository repository;
+	private MovieDTO movieDTO;
+	private MovieEntity movie;
+	private String title;
+
+	@BeforeEach
+	public void setup() throws Exception {
+		title = "MIB";
+		movieDTO = MovieFactory.createMovieDTO();
+		movie = MovieFactory.createMovieEntity();
+		PageImpl<MovieEntity> page = new PageImpl<>(List.of(movie));
+
+		Mockito.when(repository.searchByTitle(any(), any())).thenReturn(page);
+	}
 	
 	@Test
 	public void findAllShouldReturnPagedMovieDTO() {
+		Pageable pageable = PageRequest.of(0, 12);
+		Page<MovieDTO> result = service.findAll(title, pageable);
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getSize(), 1);
 	}
 	
 	@Test
